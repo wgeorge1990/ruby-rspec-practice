d0f708ae5a9136c3d657bee82b16793171b7c486
wgeorge1990 <williameverettgeorge@icloud.com>
2019-04-24 01:51:01 -0400

    Commented RSPEC

--- spec/ruby_spec.rb
+++ spec/ruby_spec.rb
@@ -1,11 +1,11 @@
 require './ruby.rb'
-
+# TDD Testing simple addition function.
 describe 'add' do
     it "returns the sum of number one and number two" do
         expect(add(4, 5)).to eq(9)
     end
 end
-
+# TDD Testing users name input to matching name in interpolated output.
 describe 'whats_your_name' do
     it "returns the name that was imput" do
         expect(whats_your_name(name = gets.chomp)).to eq("Hello, nice to meet you #{name}")
