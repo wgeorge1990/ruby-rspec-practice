49b5d87c9103118e1dfc637cb9be2eb5887f0258
wgeorge1990 <williameverettgeorge@icloud.com>
2019-04-26 09:40:13 -0400

    Fizzbuzz Rspec and solution added

--- ruby.rb
+++ ruby.rb
@@ -1,49 +1,60 @@
-#practice
 
+  def whats
+  puts "what is going on"
+  end
 
-def whats
-puts "what is going on"
-end
+  def time
+    puts Time.now
+  end
 
-def time
-  puts Time.now
-end
+  def greeting(name)
+    puts "hello my name is #{name}"
+  end
 
-def greeting(name)
-  puts "hello my name is #{name}"
-end
+  def mutiply(num1, num2)
+    puts num1 * num2
+    return num1 * num2 * 2
+  end
 
-def mutiply(num1, num2)
-  puts num1 * num2
-  return num1 * num2 * 2
-end
+  def welcome
+      puts 'hey whats your name'
+      name = gets.chomp
+      puts "nice to meet you #{name}, what would you like to do next?"
+      puts "sing song or do dance?"
+      answer = gets.chomp
+      if answer == 'sing' 
+        puts 'we are singing yay'
+      else if answer == 'dance' 
+        puts 'we are dancing'
+      else
+        puts 'thats not an option'
+      end
+    end
+  end
 
-def welcome
-    puts 'hey whats your name'
+  def add(num1, num2)
+    num1 + num2
+  end
+
+  def whats_your_name(name)
+    puts 'what is your name'
     name = gets.chomp
-    puts "nice to meet you #{name}, what would you like to do next?"
-    puts "sing song or do dance?"
-    answer = gets.chomp
-    if answer == 'sing' 
-      puts 'we are singing yay'
-    else if answer == 'dance' 
-      puts 'we are dancing'
-    else
-      puts 'thats not an option'
+    "Hello, nice to meet you #{name}"
+  end
+  
+    def fizzbuzz(num)
+      if num % 3 == 0 && num % 5 == 0
+        return 'FizzBuzz'
+      else if num % 3 == 0
+        return 'Fizz'
+      else if num % 5 == 0
+        return 'Buzz'
+      else
+        return nil
+      end
     end
   end
 end
-
-def add(num1, num2)
-  num1 + num2
-end
-
-def whats_your_name(name)
-  puts 'what is your name'
-  name = gets.chomp
-  "Hello, nice to meet you #{name}"
-end
-
 
 
 
--- spec/ruby_spec.rb
+++ spec/ruby_spec.rb
@@ -1,13 +1,37 @@
 require './ruby.rb'
+
 # TDD Testing simple addition function.
-describe 'add' do
-    it "returns the sum of number one and number two" do
-        expect(add(4, 5)).to eq(9)
+# describe 'add' do
+#     it "returns the sum of number one and number two" do
+#         expect(add(4, 5)).to eq(9)
+#     end
+# end
+# # TDD Testing users name input to matching name in interpolated output.
+# describe 'whats_your_name' do
+#     it "returns the name that was imput" do
+#         expect(whats_your_name(name = gets.chomp)).to eq("Hello, nice to meet you #{name}")
+#     end
+# end
+
+describe "fizzbuzz" do
+    it 'returns "Fizz" when the number is divisible by 3' do
+      fizz_3 = fizzbuzz(3)
+   
+      expect(fizz_3).to eq("Fizz")
     end
-end
-# TDD Testing users name input to matching name in interpolated output.
-describe 'whats_your_name' do
-    it "returns the name that was imput" do
-        expect(whats_your_name(name = gets.chomp)).to eq("Hello, nice to meet you #{name}")
+    it 'returns "Buzz" when the number is divisible by 5' do
+      fizz_5 = fizzbuzz(5)
+   
+      expect(fizz_5).to eq("Buzz")
     end
-end
+    it 'returns "FizzBuzz" when the number is divisible by 3 and 5' do
+      fizz_15 = fizzbuzz(15)
+   
+      expect(fizz_15).to eq("FizzBuzz")
+    end
+    it 'returns nil when the number is not divisible by 3 or 5' do
+      fizz_4 = fizzbuzz(4)
+   
+      expect(fizz_4).to eq(nil)
+    end
+  end
